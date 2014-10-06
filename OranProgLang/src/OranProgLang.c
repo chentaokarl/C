10f1c34c3494c9ae03eaf22e7a302913ab96c14f
chentaokarl <taokarlchen@gmail.com>
2014-10-06 14:22:24 -0500

    first commit

---
+++ OranProgLang/src/OranProgLang.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : OranProgLang.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
+
+void A(){
+	pringtf("Enter ")
+}
